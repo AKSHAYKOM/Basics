03e3afc7647bde848e567538679b0401d8f41fd4
Akshay <komaraakshay1@gmail.com>
2026-03-16 02:07:33 +0530

    Solved the Second one

---
+++ Bitwise_operator1/Second.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pp = 2, qq = 9, rr = 5;
+    
+    pp = (pp + rr) + pp;
+    rr = rr + rr;
+    rr= (rr + rr) & rr;
+
+    cout << pp + qq + rr << endl;  
+
+    return 0;
+}
